061268174d6f1b8199f3a80fa78762cc994e9f86
pauladdai <pauladdai19@augustana.edu>
2023-02-04 22:40:39 -0600

    Initial Commit

---
+++ maps/mapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapsApp.swift
+//  maps
+//
+//  Created by Paul Addai on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct mapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
