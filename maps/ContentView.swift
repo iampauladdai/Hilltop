f20573066e4200e2486b69cb87d06e4f0f20f506
pauladdai <pauladdai19@augustana.edu>
2023-02-04 23:02:21 -0600

    add directions to two cordinates

--- maps/ContentView.swift
+++ maps/ContentView.swift
@@ -6,21 +6,55 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        MapView()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+class MapDelegate: NSObject, MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.yellow
+        renderer.lineWidth = 5.0
+        return renderer
     }
+}
+
+struct MapView: UIViewRepresentable {
+    let mapDelegate = MapDelegate()
+    func makeUIView(context: Context) -> MKMapView {
+        let map = MKMapView(frame: .zero)
+        map.delegate = mapDelegate
+        return map
+    }
+
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(
+            latitude: 37.7749, longitude: -122.4194)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+        
+        let source = CLLocationCoordinate2D(latitude: 37.7749, longitude: -122.4194)
+        let destination = CLLocationCoordinate2D(latitude: 37.7858, longitude: -122.4064)
+        
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: MKPlacemark(coordinate: source))
+        request.destination = MKMapItem(placemark: MKPlacemark(coordinate: destination))
+        
+        let directions = MKDirections(request: request)
+
+        directions.calculate { (response, error) in
+                    guard let response = response else { return }
+                    for route in response.routes {
+                        uiView.addOverlay(route.polyline)
+                        uiView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+                    }
+                }
+        
+    }
+
 }
